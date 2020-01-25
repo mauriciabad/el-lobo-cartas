59dfa779bd186b236222f0cd592ea14b5b1aaa5b
Maurici <hello@mauriciabad.com>
2020-01-25 18:18:55 +0100

    version 2

---
+++ src/main.js
@@ -0,0 +1,70 @@
+// ------ CODE -------- //
+
+const fs = require('fs');
+const pdf = require('html-pdf');
+
+const cards = {
+  normal: fs.readdirSync('./src/img/normal').filter((fileName) => /\.(svg|png|jpg|jpeg|gif)$/i.test(fileName) ),
+  events: JSON.parse(fs.readFileSync('data/events.json')),
+}
+
+
+var html = `<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>Lobo</title>
+    <link href="main.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Lora:400,400i,700,700i" rel="stylesheet">
+  </head>
+  <body>
+    ${generateCards(cards)}
+  </body>
+</html>`;
+
+fs.writeFile("dist/index.html", html, (err) => {
+  if (err) console.error(err);
+}); 
+
+// let options = {
+//   "orientation": "landscape",
+//   "border": "1cm",
+//   "format": "A3"
+// }
+// pdf.create(html, options).toFile('./dist/cards.pdf', (err, res) => {
+//   if (err) console.error(err);
+// });
+
+
+
+// ----------- FUNCTIONS ------------ //
+
+function generateCards(cards){
+  // Explanation: for each type of card we generate and combine the html of each card.
+  // Note: the order of generation is important to match the front widhth the backs.
+  return `
+  <div class="deck">
+    ${cards['normal'].reduce((total, card) => total + generateNormalCard(card), '')}
+  </div>
+  <div class="deck">
+    ${cards['events'].reduce((total, card) => total + generateEventCard(card), '')}
+  </div>`;
+}
+
+function generateNormalCard(src) {
+  return `
+    <div class="card card--normal">
+      <img src="img/normal/${src}" class="card__img">
+    </div>`;
+}
+
+function generateEventCard(info) {
+  return `
+    <div class="card card--event">
+      <img src="img/events/${info.duration}.svg" class="card__img">
+      <h2 class="card__title">${info.title}</h2>
+      <p class="card__description">${info.description}</p>
+    </div>`;
+}
+
+
